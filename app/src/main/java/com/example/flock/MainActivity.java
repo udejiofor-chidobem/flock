b1514585582842529666929b8ac63e4309020b56
Chidobem Udejiofor <cudejiofor6@gatech.edu>
2022-10-02 09:27:02 -0400

    Stable Build, New UI: 1

--- app/src/main/java/com/example/flock/MainActivity.java
+++ app/src/main/java/com/example/flock/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.flock;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.intersystems.jdbc.IRISDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -14,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -22,6 +29,20 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.journal);
 
+        String dbURL = "jdbc:IRIS://83372d1a-a3d12a76-d6e2cfe457-aadc9b2654b35456.elb.us-east-2.amazonaws.com:1972/USER";
+        String user = "SQLAdmin";
+        String pass = "G5nmYNAT2syVtLL!";
+
+        IRISDataSource ds = new IRISDataSource();
+        try {
+            ds.setURL(dbURL);
+            ds.setUser(user);
+            ds.setPassword(pass);
+            Connection dbconnection = ds.getConnection();
+            Log.i("Success","Connected to InternSystemsIris via JDBC.");
+        } catch (SQLException e) {
+            Log.e("Failed",e.getMessage());
+        }
     }
     Journal journal = new Journal();
     Record record = new Record();
